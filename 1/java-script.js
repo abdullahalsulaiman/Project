816e4625d12cf39fbd391c7491e6712b9cd4901d
abdullahalsulaiman <86481808+abdullahalsulaiman@users.noreply.github.com>
2021-06-25 17:38:21 +0300

    Add files via upload

---
+++ 1/java-script.js
@@ -0,0 +1,11 @@
+var order_list = new Array();
+		
+		function addToBasket(x){
+			order_list.push(x);
+			var y = "1";
+			document.getElementById('cart').style.backgroundImage = "url(img/02.png)";
+			var item_count = document.getElementById("items-No").textContent;
+			var value = item_count + "+"+y;
+			document.getElementById("items-No").innerHTML = eval(value);
+			
+		}
